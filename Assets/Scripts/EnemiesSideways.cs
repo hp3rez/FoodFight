056e9f84c6f1d7eaef4b12a518f99e7a6eeeef14
Hannah Perez <70717380+hp3rez@users.noreply.github.com>
2023-10-18 18:16:03 -0500

    Created saw and heart prefabs
    Made a saw trap that deals damage to the player and a heart object that heals the player.

--- Assets/Scripts/EnemiesSideways.cs
+++ Assets/Scripts/EnemiesSideways.cs
@@ -4,12 +4,38 @@ using UnityEngine;
 
 public class EnemiesSideways : MonoBehaviour
 {
+    
+    [SerializeField] private float moveDist;
+    [SerializeField] private float speed;
     [SerializeField] private float damage;
+    private float leftEdge;
+    private float rightEdge;
+    private bool movingLeft;
+
+    private void Awake() {
+        leftEdge = transform.position.x - moveDist;
+        rightEdge = transform.position.x + moveDist;
+    }
+
+    private void Update() {
+        if(movingLeft) {
+            if(transform.position.x > leftEdge) {
+                transform.position = new Vector3(transform.position.x - speed * Time.deltaTime, transform.position.y, transform.position.z);
+            } else {
+                movingLeft = false;
+            }
+        } else{
+            if(transform.position.x < rightEdge) {
+                transform.position = new Vector3(transform.position.x + speed * Time.deltaTime, transform.position.y, transform.position.z);
+            } else {
+                movingLeft = true;
+            }
+        }
+    }
 
     private void OnTriggerEnter2D(Collider2D collision) {
         if (collision.tag == "Player") {
-            Health playerHealth = collision.GetComponent<Health>();
-            playerHealth.TakeDamage(0.1f);
+            collision.GetComponent<Health>().TakeDamage(damage);
         }
     }
 }
